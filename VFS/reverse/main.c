4e5d5c67d3e341b19bf8157aaa90c4f7b347326d
Dmitry Rodionov <dmitri1998171@icloud.com>
2020-07-24 13:29:50 +0700

    Complete reverse(VFS) project

--- VFS/reverse/main.c
+++ VFS/reverse/main.c
@@ -4,15 +4,20 @@
 #include <stdlib.h>
 
 int main(){
-    int fd, ret;
+    int fd, ret, i=0;
     char value;
 
     if ((fd = open("text.txt", O_RDONLY))== -1){
         perror("open failed!"); exit(1);}
 
-    while ((ret = read(fd, &value, 1)) > 0){
-		putchar(value);
-	}
+
+    int length = lseek (fd, 0, SEEK_END);
+
+    for(int i = length-1; i >= 0; i--){
+        lseek (fd, i, SEEK_SET);
+        ret = read(fd, &value, 1);
+        putchar(value);
+    }
 
     close(fd);
     return 0;
