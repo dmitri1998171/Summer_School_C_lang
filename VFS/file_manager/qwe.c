509ef1102d5d4845514bea2359b8c39f7b07f9dd
Dmitry Rodionov <dmitri1998171@icloud.com>
2020-07-27 13:47:10 +0700

    add Interface

--- VFS/file_manager/qwe.c
+++ VFS/file_manager/qwe.c
@@ -1,43 +1,39 @@
 #include <menu.h>
-#include <string.h>
-#include <stdlib.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 	4
 
 char *choices[] = {
                         "Choice 1",
                         "Choice 2",
                         "Choice 3",
                         "Choice 4",
-                        "Choice 1",
-                        "Choice 2",
-                        "Choice 3",
-                        "Choice 4",
-                        "Choice 1",
-                        "Choice 2",
-                        "Choice 3",
-                        "Choice 4",
+			"Choice 5",
+			"Choice 6",
+			"Choice 7",
+			"Choice 8",
+			"Choice 9",
+			"Choice 10",
                         "Exit",
                         (char *)NULL,
                   };
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 
 int main()
-{	
-    ITEM **my_items;
+{	ITEM **my_items;
 	int c;				
 	MENU *my_menu;
-        WINDOW *win_left;
-        WINDOW *win_right;
+        WINDOW *my_menu_win;
         int n_choices, i;
 	
 	/* Initialize curses */
 	initscr();
 	start_color();
-    cbreak();
-    noecho();
+        cbreak();
+        noecho();
 	keypad(stdscr, TRUE);
 	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_CYAN, COLOR_BLACK);
 
 	/* Create items */
         n_choices = ARRAY_SIZE(choices);
@@ -48,45 +44,36 @@ int main()
 	/* Crate menu */
 	my_menu = new_menu((ITEM **)my_items);
 
-	/* Create the windows to be associated with the menu */
-        win_left = newwin(LINES-1, COLS/2, 0, 0);
-        keypad(win_left, TRUE);
+	/* Create the window to be associated with the menu */
+        my_menu_win = newwin(10, 40, 4, 4);
+        keypad(my_menu_win, TRUE);
      
-        win_right = newwin(LINES-1, COLS/2, 0, COLS/2);
-        keypad(win_right, TRUE);
-
 	/* Set main window and sub window */
-        set_menu_win(my_menu, win_left);
-        set_menu_sub(my_menu, derwin(win_left, 6, 38, 3, 1));
-
-        // set_menu_win(my_menu, win_right);
-        // set_menu_sub(my_menu, derwin(win_right, 6, 38, 3, 1));
-
+        set_menu_win(my_menu, my_menu_win);
+        set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 3, 1));
+	set_menu_format(my_menu, 5, 1);
+			
 	/* Set menu mark to the string " * " */
         set_menu_mark(my_menu, " * ");
 
 	/* Print a border around the main window and print a title */
-	print_in_middle(win_left, 1, 0, COLS/2, "File Manager", COLOR_PAIR(1));
-    print_in_middle(win_right, 1, 0, COLS/2, "File Manager", COLOR_PAIR(1));
-
-    box(win_left, 0, 0);
-	mvwaddch(win_left, 2, 0, ACS_LTEE);
-	mvwhline(win_left, 2, 1, ACS_HLINE, (COLS/2)-2);
-	mvwaddch(win_left, 2, (COLS/2)-1, ACS_RTEE);
+        box(my_menu_win, 0, 0);
+	print_in_middle(my_menu_win, 1, 0, 40, "My Menu", COLOR_PAIR(1));
+	mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
+	mvwhline(my_menu_win, 2, 1, ACS_HLINE, 38);
+	mvwaddch(my_menu_win, 2, 39, ACS_RTEE);
         
-    box(win_right, 0, 0);
-    mvwaddch(win_right, 2, (COLS/2)-1, ACS_RTEE);
-	mvwhline(win_right, 2, 1, ACS_HLINE, (COLS/2)-2);
-	mvwaddch(win_right, 2, 0, ACS_LTEE);
-	mvprintw(LINES - 1, 1, "Tab - switch panel  F1 - exit");
-	refresh();
-    
 	/* Post the menu */
 	post_menu(my_menu);
-	wrefresh(win_left);
-    wrefresh(win_right);
+	wrefresh(my_menu_win);
+	
+	attron(COLOR_PAIR(2));
+	mvprintw(LINES - 2, 0, "Use PageUp and PageDown to scoll down or up a page of items");
+	mvprintw(LINES - 1, 0, "Arrow Keys to navigate (F1 to Exit)");
+	attroff(COLOR_PAIR(2));
+	refresh();
 
-	while((c = wgetch(win_left)) != KEY_F(1))
+	while((c = wgetch(my_menu_win)) != KEY_F(1))
 	{       switch(c)
 	        {	case KEY_DOWN:
 				menu_driver(my_menu, REQ_DOWN_ITEM);
@@ -94,14 +81,14 @@ int main()
 			case KEY_UP:
 				menu_driver(my_menu, REQ_UP_ITEM);
 				break;
-            case KEY_NPAGE:
+			case KEY_NPAGE:
 				menu_driver(my_menu, REQ_SCR_DPAGE);
 				break;
 			case KEY_PPAGE:
 				menu_driver(my_menu, REQ_SCR_UPAGE);
 				break;
 		}
-                wrefresh(win_left);
+                wrefresh(my_menu_win);
 	}	
 
 	/* Unpost and free all the memory taken up */
@@ -116,6 +103,8 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
 {	int length, x, y;
 	float temp;
 
+	if(win == NULL)
+		win = stdscr;
 	getyx(win, y, x);
 	if(startx != 0)
 		x = startx;
