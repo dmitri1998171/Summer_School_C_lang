847956ca8f024d340cab8f80b4f7388401ceccdb
dmitri1998171 <dmitri1998171@icloud.com>
2020-08-05 07:46:34 +0700

    Complete Fork task 1 and 2

--- Fork/2.c
+++ Fork/2.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(){
-    pid_t pid;
+    pid_t pid1, pid2, pid3, pid4, pid5;
     int status;
 
-    pid = fork();
-
-    if(pid == 0){
-        printf("This is a child: %d\n", getpid());
-        getchar();
+    pid1 = fork();
+    if(pid1 == 0){
+        printf("child 1:\tpid=%d\tppid=%d\n", getpid(), getppid());
+        pid5 = fork();
+        if(pid5 == 0){
+            printf("child 5:\tpid=%d\tppid=%d\n", getpid(), getppid());
+            exit(0);
+        }
         exit(0);
     }
-    else{
-        wait(&status);
-        printf("This is a parent: %d\n", getpid());
+
+    pid2 = fork();
+    if(pid2 == 0){
+        printf("child 2:\tpid=%d\tppid=%d\n", getpid(), getppid());
+        pid3 = fork();
+        if(pid3 == 0){
+            printf("child 3:\tpid=%d\tppid=%d\n", getpid(), getppid());
+            exit(0);
+        }
+        pid4 = fork();
+        if(pid4 == 0){
+            printf("child 4:\tpid=%d\tppid=%d\n", getpid(), getppid());
+            exit(0);
+        }
+        exit(0);
     }
+
+    waitpid(pid1, &status, 0);
+    waitpid(pid2, &status, 0);
+    waitpid(pid3, &status, 0);
+    waitpid(pid4, &status, 0);
+    waitpid(pid5, &status, 0);
+    printf("parent1: %d\n", getpid());
+    // getchar();
+    
     return 0;
 }
