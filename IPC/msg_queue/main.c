d16cba7adb55b6e0c6d4b2ef8e6bfabe320c2f3e
dmitri1998171 <dmitri1998171@icloud.com>
2020-08-18 12:58:44 +0700

    Initial commit

---
+++ IPC/msg_queue/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+
+int main(){
+    key_t key;
+    
+
+    return 0;
+}
