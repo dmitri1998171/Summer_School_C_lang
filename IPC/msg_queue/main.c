f7dc1c055b8ea6347385b03b04fc1d2c90cf2260
dmitri1998171 <dmitri1998171@icloud.com>
2020-08-18 13:56:25 +0700

    code review

--- IPC/msg_queue/main.c
+++ IPC/msg_queue/main.c
@@ -6,50 +6,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MSGSZ     128
+#define MSGSZ 128
 
-typedef struct msgbuf {
-         long    mtype;
-         char    mtext[MSGSZ];
-         } message_buf;
+typedef struct msgbuf{
+    long mtype;
+    char mtext[MSGSZ];
+} message_buf;
 
 int main(){
     int msqid;
     int msgflg = IPC_CREAT | 0666;
-    key_t key;
+    key_t key=10;
     message_buf sbuf;
     size_t buf_length;
 
-    key = 10;
-
-    printf("Calling msgget with key %#lx and flag %#o\n",key,msgflg);
-
     if ((msqid = msgget(key, msgflg )) < 0) {
         perror("msgget");
-        exit(1);
-    }
-    else 
-     printf("msgget: msgget succeeded: msqid = %d\n", msqid);
-
+        exit(1); }
 
     sbuf.mtype = 1;
-    
-    printf("msgget: msgget succeeded: msqid = %d\n", msqid);
-    
-    (void) strcpy(sbuf.mtext, "I am in the queue?");
-    
-    printf("msgget: msgget succeeded: msqid = %d\n", msqid);
-    
+    (void) strcpy(sbuf.mtext, "Hello");
     buf_length = strlen(sbuf.mtext) + 1 ;
     
     if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0) {
        printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
         perror("msgsnd");
-        exit(1);
-    }
+        exit(1); }
 
-   else 
-      printf("Message: \"%s\" Sent\n", sbuf.mtext);
+    else printf("Message Sent\n");
       
     exit(0);
 }
--- IPC/msg_queue/main1.c
+++ IPC/msg_queue/main1.c
@@ -5,31 +5,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MSGSZ     128
+#define MSGSZ 128
 
 typedef struct msgbuf {
-    long    mtype;
-    char    mtext[MSGSZ];
+    long mtype;
+    char mtext[MSGSZ];
 } message_buf;
 
 
 int main(){
     int msqid;
-    key_t key;
+    key_t key = 10;
     message_buf  rbuf;
-
-    key = 10;
 
     if ((msqid = msgget(key, 0666)) < 0) {
         perror("msgget");
-        exit(1);
-    }
+        exit(1); }
 
-    
     if (msgrcv(msqid, &rbuf, MSGSZ, 1, 0) < 0) {
         perror("msgrcv");
-        exit(1);
-    }
+        exit(1);}
 
     printf("%s\n", rbuf.mtext);
     exit(0);
