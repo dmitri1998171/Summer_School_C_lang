eda755a8205d84f5e05c909fa234816f7aa88a69
dmitri1998171 <dmitri1998171@icloud.com>
2020-08-18 16:31:06 +0700

    add redirect stdout in to pipe and reverse

--- IPC/pipe/main.c
+++ IPC/pipe/main.c
@@ -23,14 +23,13 @@ int main(){
     char c2[15][15]; // аргументы 1-ой команды
     char c3[15][15]; // аргументы 2-ой команды
     char *t1, *t2, *t3;
-    int i=0, j=0, k=0, fd_fifo;
+    int i=0, j=0, k=0;
+    int stat;
+    int mypipe[2];
     pid_t pid1, pid2;
 
     printf("\n$: ");
-    gets(str);
-
-    int mypipe[2];
-    
+    gets(str);    
 
     // разбиваем строку на команды
     t1 = strtok(str, "|");
@@ -59,37 +58,34 @@ int main(){
         k++;
     }
 
-    // exit(0);
-
     printf("\n");
     
-    /* Create the pipe. */
+    // создаем канал
     if (pipe (mypipe)){
         fprintf(stderr, "Pipe failed.\n");
         return EXIT_FAILURE;}
 
     pid1 = fork();
-    if(pid1 == 0){
+
+    if(pid1 == 0){            // команда 1
         close(mypipe[0]);
         dup2(mypipe[1], 1);     // STDOUT в pipe
 
         execlp(c2[0], c2[0], c2[1], NULL);
-        close(mypipe[1]);
         exit(0);
     }
 
     pid2 = fork();
-    if(pid2 == 0){
+    if(pid2 == 0){            // команда 2
         close(mypipe[1]);
-        dup2(1, mypipe[0]);
+        dup2(mypipe[0], 0);     // pipe в STDIN
 
-        execl(c3[0], c3[0], c3[1], NULL);
-        close(mypipe[0]);
+        execlp(c3[0], c3[0], c3[1], NULL);
         exit(0);
     }
 
-    wait(&pid1);
-    wait(&pid2);
+    waitpid(pid1, &stat, 0);
+    waitpid(pid2, &stat, 0);
 
     return 0;
 }
