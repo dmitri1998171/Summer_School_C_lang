5066607d6fcdd1008d02ab44efffa1c455d07296
Dmitry <dmitri1998171@icloud.com>
2020-09-03 11:59:48 +0700

    complete ePoll task

---
+++ IPC/socket/5_tasks/4/ePoll/server.c
@@ -0,0 +1,105 @@
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <sys/epoll.h> 
+
+#define MAX_EVENTS 1 
+#define MAX_BUF    1024 
+
+// Simple single thread epoll server, that prints incoming requests to the stdout. 
+int main() 
+{ 
+    int s; 
+    // create socket 
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) { 
+        perror("socket error"); 
+        exit(1); 
+    } 
+
+    struct sockaddr_in serv_addr; 
+    int portno = 1133; 
+    serv_addr.sin_family = AF_INET; // don't care IPv4 or IPv6 
+    serv_addr.sin_addr.s_addr = INADDR_ANY; // receive packets destined to any of the interfaces 
+    serv_addr.sin_port = htons(portno); 
+
+    // by default, system doesn't close socket, but set it's status to TIME_WAIT; allow socket reuse    
+    int yes = 1; 
+    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) { 
+        perror("setsockopt"); 
+        exit(1); 
+    }  
+
+    // bind socket to address 
+    if (bind(s, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) { 
+        perror("bind error"); 
+        close(s); 
+        exit(1); 
+    } 
+
+    // start listening on port 7000, allow only 1 connection in the incominq queue 
+    if (listen(s, 1) == -1) { 
+        perror("listen error"); 
+        close(s); 
+        exit(1); 
+    } 
+
+    // create epoll instance 
+    int epfd; 
+    epfd = epoll_create(5); 
+    if (epfd == -1) { 
+        perror("epoll_create"); 
+    } 
+
+    int cs; // client socket 
+    struct sockaddr_storage their_addr; 
+    socklen_t addr_size = sizeof their_addr; 
+
+    char request[MAX_BUF] = ""; 
+    struct epoll_event evlist[MAX_EVENTS]; 
+    while (strstr(request, "close") == NULL) 
+    { 
+        // wait for incoming connection 
+        if ((cs = accept(s, (struct sockaddr *) &their_addr, &addr_size)) == -1) { 
+            perror("accept error"); 
+            close(s); 
+            exit(1); 
+        } 
+
+        struct epoll_event ev; 
+        ev.data.fd = cs; 
+        ev.events = EPOLLIN; 
+        if (epoll_ctl(epfd, EPOLL_CTL_ADD, cs, &ev) == -1) { 
+            perror("epoll_ctl"); 
+        } 
+        int ready = epoll_wait(epfd, evlist, MAX_EVENTS, -1); 
+        if (ready == -1) { 
+            perror("epoll_wait"); 
+        } 
+        int j; 
+        char buf[MAX_BUF]; 
+        char send_buf[MAX_BUF+50]; 
+        for (j = 0; j < ready; j++) { 
+            int byte_count = read(evlist[j].data.fd, buf, MAX_BUF); 
+            if (byte_count == -1) { 
+                perror("read"); 
+            } 
+            memcpy(request, buf, byte_count); 
+            puts(request); 
+        }   
+
+        snprintf(send_buf, sizeof(send_buf), "%s%s", "server answer ", buf);
+        if (send(cs, send_buf, sizeof(send_buf), 0) == -1) { 
+            perror("send error"); 
+            close(s); 
+            close(cs); 
+            exit(1); 
+        } 
+        close(cs); 
+    }       
+    close(s); 
+    printf("Shutdown server... \n"); 
+}
